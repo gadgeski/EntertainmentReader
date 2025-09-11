5fae5cf0e53a297dd4113cbd863a5d3f55539d0d
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-11 09:58:35 +0900

    feat: (Sources/App/EntertainmentReaderApp.swift)

---
+++ EntertainmentReader/Sources/App/EntertainmentReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntertainmentReaderApp.swift
+//  EntertainmentReader
+//
+//  Created by Dev Tech on 2025/09/09.
+//
+
+import SwiftUI
+
+@main
+struct EntertainmentReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
